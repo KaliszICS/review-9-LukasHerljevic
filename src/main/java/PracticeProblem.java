dc90406d1aacce4abf44254c7f50d821e4cbfb57
LukasHerljevic <258844296+LukasHerljevic@users.noreply.github.com>
2026-04-21 21:12:52 +0000

    .

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,20 @@ public class PracticeProblem {
 
 	}
 
+	public static int lettersToFive(String str, char ch) {
+		int count = 0;
+		int index = 0;
+		while (index < str.length() && count < 5) {
+			if (str.charAt(index) == ch) {
+				count++;
+			}
+			index++;
+		}
+		if (count == 5) {
+			return index;
+		} else {
+			return -1;
+		}
+	}
+
 }
