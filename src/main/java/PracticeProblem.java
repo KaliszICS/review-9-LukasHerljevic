7b021a2725428fa240697413da180396899fae79
LukasHerljevic <258844296+LukasHerljevic@users.noreply.github.com>
2026-04-22 22:10:26 +0000

    e# Please enter the commit message for your changes. Lines starting

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -18,6 +18,34 @@ public class PracticeProblem {
 		} else {
 			return -1;
 		}
-	}
+
+		public static int countLetter(String text, char target) {
+    int count = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+        if (text.charAt(i) == target) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+	}public static int oddSum(int num1, int num2) {
+    
+    int start = Math.min(num1, num2);
+    int end = Math.max(num1, num2);
+    
+    int sum = 0;
+
+    for (int i = start; i <= end; i++) {
+        
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
 
 }
